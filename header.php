c6d9686748f4d41735e12045e390f80ac984c38a
Edgen Mades | Jr. Web Dev <jinmadez@gmail.com>
2025-07-18 09:59:41 +0800

    Add Astra Child Theme files

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html> 
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<header class="site-header">
+    <div class="custom-header-text">
+        <h1>This is thes	header</h1>
+    </div>
+    <?php astra_header(); ?>
+</header>
